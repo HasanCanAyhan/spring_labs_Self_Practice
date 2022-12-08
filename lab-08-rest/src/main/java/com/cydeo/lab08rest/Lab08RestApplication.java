51ae71b757490071253d4f68e2828600e611bc76
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-08 10:06:20 +0100

    spring lab practice

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/Lab08RestApplication.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/Lab08RestApplication.java
@@ -1,9 +1,14 @@
 package com.cydeo.lab08rest;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Lab08RestApplication {
@@ -14,6 +19,14 @@ public class Lab08RestApplication {
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
+    }
+
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI().info(
+                        new Info().title("Ecommerce Application OpenAPI").version("v1").description("Ecommerce Application API Documentation"))
+                .servers(List.of(new Server().url("https://cydeo.com").description("Dev Environment")));
     }
 
 }
--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/ProductController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.cydeo.lab08rest.dto.ProductRequest;
 import com.cydeo.lab08rest.entity.Category;
 import com.cydeo.lab08rest.model.ResponseWrapper;
 import com.cydeo.lab08rest.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@Tag(name = "Product",description = "Product Controller")
 public class ProductController {
 
     private final ProductService productService;
@@ -26,18 +29,21 @@ public class ProductController {
 
 
     @GetMapping
+    @Operation(summary = "Read all products")
     public ResponseEntity<ResponseWrapper> getProductList() {
         return ResponseEntity.ok(new ResponseWrapper("Products are successfully retrieved", productService.getProductList(), HttpStatus.OK));
     }
 
 
     @PostMapping
+    @Operation(summary = "Create an specific product")
     public ResponseEntity<ResponseWrapper> createProduct(@RequestBody ProductDTO productDTO) {
         ProductDTO productDTO1 = productService.save(productDTO);
         return ResponseEntity.ok(new ResponseWrapper("Product is successfully created", productDTO1, HttpStatus.OK));
     }
 
     @PostMapping("/categoryandprice")
+    @Operation(summary = "Get Category And Price ")
     public ResponseEntity<ResponseWrapper> getCategoryAndPrice(@RequestBody ProductRequest productRequest) {
 
         List<ProductDTO> productDTOS = productService.getProductRequest(productRequest);
@@ -49,6 +55,7 @@ public class ProductController {
 
 
     @PutMapping
+    @Operation(summary = "Update an specific product")
     public ResponseEntity<ResponseWrapper> updateProduct(@RequestBody ProductDTO productDTO) {
 
         ProductDTO updatedProduct = productService.update(productDTO);
@@ -59,6 +66,7 @@ public class ProductController {
 
 
     @GetMapping("/{name}")
+    @Operation(summary = "Get Product By Name")
     public ResponseEntity<ResponseWrapper> getProductByName(@PathVariable("name") String name) {
 
         ProductDTO productDTO = productService.getProductByName(name);
@@ -68,6 +76,7 @@ public class ProductController {
 
 
     @GetMapping("/top3")
+    @Operation(summary = "Read Top 3 Products")
     public ResponseEntity<ResponseWrapper> getTop3ProductList() {
 
         List<ProductDTO> top3ProductList = productService.getTop3ProductList();
@@ -77,6 +86,7 @@ public class ProductController {
 
 
     @GetMapping("/price/{price}")
+    @Operation(summary = "Read Products' count  By Price")
     public ResponseEntity<ResponseWrapper> getProductListByPrice(@PathVariable("price") BigDecimal price) { // it is asking the count greater than specific price
         Integer count = productService.getProductListByPrice(price);
         return ResponseEntity.ok(new ResponseWrapper("Products are successfully retrieved", count, HttpStatus.OK));
@@ -85,6 +95,7 @@ public class ProductController {
 
 
     @GetMapping("/price/{price}/quantity/{quantity}")
+    @Operation(summary = "Read Products By Price And Quantity")
     public ResponseEntity<ResponseWrapper> getProductListByPriceAndQuantity(@PathVariable("price") BigDecimal price, @PathVariable("quantity") Integer quantity) {
 
         List<ProductDTO> productDTOS = productService.getProductListByPriceAndQuantity(price, quantity);
@@ -94,11 +105,11 @@ public class ProductController {
 
 
     @GetMapping("/category/{id}")
+    @Operation(summary = "Read Products By Category")
     public ResponseEntity<ResponseWrapper> getProductListByCategory(@PathVariable("id") Long id) {
         List<ProductDTO> productDTOList = productService.getProductListByCategory(id);
 
         return ResponseEntity.ok(new ResponseWrapper("Products are successfully retrieved", productDTOList, HttpStatus.OK));
     }
-
 
 }
